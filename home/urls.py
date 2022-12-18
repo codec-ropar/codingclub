7aee550bd49855171204ca3171476fd4201d1430
TheViking733n <anant733n@gmail.com>
2022-12-18 13:00:45 +0530

    add login page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def login(request):
+    return render(request, 'home/login.html')

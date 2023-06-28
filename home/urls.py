1a4a5cfd6944190b6903d6498814e0e723a4a90d
TheViking733n <anant733n@gmail.com>
2023-06-28 19:33:33 +0530

    removed slash from urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('team/', views.team, name='team'),
-    path('coders/', views.coders, name='coders'),
-    path('events/', views.events, name='events'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('team', views.team, name='team'),
+    path('coders', views.coders, name='coders'),
+    path('events', views.events, name='events'),
     path('alumni', views.alumni, name='alumni'),
 ]

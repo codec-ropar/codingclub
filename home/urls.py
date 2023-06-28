1e2ef5bf5c78ada5ff9b3fd9a0f4c4edc7e792dc
TheViking733n <anant733n@gmail.com>
2023-06-28 19:43:27 +0530

    added slash path in urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,19 @@ from . import views
 
 urlpatterns = [
     path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('signup', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('team', views.team, name='team'),
+    path('team/', views.team, name='team'),
     path('coders', views.coders, name='coders'),
+    path('coders/', views.coders, name='coders'),
     path('events', views.events, name='events'),
+    path('events/', views.events, name='events'),
     path('alumni', views.alumni, name='alumni'),
+    path('alumni/', views.alumni, name='alumni'),
 ]

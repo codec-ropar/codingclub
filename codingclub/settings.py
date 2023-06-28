0145b96030aaec6ad8aaf5ff93519e88c34039c9
TheViking733n <anant733n@gmail.com>
2023-06-28 19:29:52 +0530

    removed sqllite3 database

--- codingclub/settings.py
+++ codingclub/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'codingclub.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 

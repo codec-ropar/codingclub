d13ddc3bfa4819b7549a7db3e9ec4f4d4e7b7518
TheViking733n <anant733n@gmail.com>
2023-07-04 18:51:53 +0530

    test

--- codingclub/settings.py
+++ codingclub/settings.py
@@ -15,7 +15,7 @@ import os
 import dotenv
 import dj_database_url
 dotenv.load_dotenv()
-
+print(os.environ.get('DATABASE_URL'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
